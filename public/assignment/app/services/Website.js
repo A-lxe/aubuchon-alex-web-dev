98502fb855c606ec098ab6bef4f235c6d74b3a0d
A-lxe <alex@alxe.co>
2016-05-29 18:48:50 -0400

    Website service complete!

---
+++ public/assignment/app/services/Website.js
@@ -0,0 +1,82 @@
+(function() {
+    function Website() {
+        var websites = [
+            { "_id": "123", "name": "Facebook",    "developerId": "456" },
+            { "_id": "234", "name": "Tweeter",     "developerId": "456" },
+            { "_id": "456", "name": "Gizmodo",     "developerId": "456" },
+            { "_id": "567", "name": "Tic Tac Toe", "developerId": "123" },
+            { "_id": "678", "name": "Checkers",    "developerId": "123" },
+            { "_id": "789", "name": "Chess",       "developerId": "234" }
+        ];
+
+        function createWebsite(userId, website) {
+            var newSite = angular.copy(website);
+            newSite.developerId = userId;
+            newSite._id = newId();
+        }
+
+        function newId() {
+            var temp = 0;
+            for(var i in websites) {
+                temp = Math.max(websites[i]._id, temp);
+            }
+            return temp + 1;
+        }
+
+        function findWebsitesByUser(userId) {
+            var sites = []
+            for(var i in websites) {
+                if(websites[i].developerId == userId) {
+                    sites.push(angular.copy(websites[i]));
+                }
+            }
+            return sites;
+        }
+
+        function findWebsiteById(websiteId) {
+            for(var i in websites) {
+                if(websites[i]._id == websiteId) {
+                    return angular.copy(websites[i]);
+                }
+            }
+        }
+
+        function updateWebsite(websiteId, website) {
+            for(var i in websites) {
+                if(websites[i]._id == websiteId) {
+                    websites[i].name = website.name;
+                    websites[i].description = website.description;
+                }
+            }
+        }
+
+        function deleteWebsite(websiteId) {
+            for(var i in websites) {
+                if(websites[i]._id == websiteId) {
+                    websites.splice(i,1);
+                }
+            }
+        }
+
+        function websiteWithName(name) {
+            for(var i in websites) {
+                if(websites[i].name == name) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        return {
+            createWebsite: createWebsite,
+            findWebsiteById: findWebsiteById,
+            findWebsitesByUser: findWebsitesByUser,
+            updateWebsite: updateWebsite,
+            deleteWebsite: deleteWebsite,
+            websiteWithName: websiteWithName
+        }
+    }
+
+    angular.module('App')
+        .factory('Website', Website)
+})();
