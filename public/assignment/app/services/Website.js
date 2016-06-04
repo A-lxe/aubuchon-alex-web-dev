a3dc024ce3b880aa9a2174c0deb5e4c0c90f25be
A-lxe <alex@alxe.co>
2016-06-04 18:31:58 -0400

    Website client service complete

--- public/assignment/app/services/Website.js
+++ public/assignment/app/services/Website.js
@@ -1,76 +1,28 @@
 (function() {
     function Website() {
-        var websites = [
-            { "_id": "123", "name": "Facebook",    "developerId": "456" },
-            { "_id": "234", "name": "Tweeter",     "developerId": "456" },
-            { "_id": "456", "name": "Gizmodo",     "developerId": "456" },
-            { "_id": "567", "name": "Tic Tac Toe", "developerId": "123" },
-            { "_id": "678", "name": "Checkers",    "developerId": "123" },
-            { "_id": "789", "name": "Chess",       "developerId": "234" }
-        ];
-
         function createWebsite(userId, website) {
-            if(websiteWithName(userId, website.name)) {
-                return false;
-            }
-            var newSite = angular.copy(website);
-            newSite.developerId = userId;
-            newSite._id = newId();
-            websites.push(newSite);
-            return newSite;
-        }
-
-        function newId() {
-            var temp = 0;
-            for(var i in websites) {
-                temp = Math.max(websites[i]._id, temp);
-            }
-            return temp + 1;
+            var url = "/api/user/" + userId + "/website";
+            return $http.post(url, website);
         }
 
         function findWebsitesByUser(userId) {
-            var sites = []
-            for(var i in websites) {
-                if(websites[i].developerId == userId) {
-                    sites.push(angular.copy(websites[i]));
-                }
-            }
-            return sites;
+            var url = "/api/user/" + userId + "/website";
+            return $http.get(url);
         }
 
         function findWebsiteById(websiteId) {
-            for(var i in websites) {
-                if(websites[i]._id == websiteId) {
-                    return angular.copy(websites[i]);
-                }
-            }
+            var url = "/api/website/" + websiteId;
+            return $http.get(url);
         }
 
         function updateWebsite(websiteId, website) {
-            for(var i in websites) {
-                if(websites[i]._id == websiteId) {
-                    websites[i].name = website.name;
-                    websites[i].description = website.description;
-                    return angular.copy(websites[i]);
-                }
-            }
+            var url = "/api/website/" + websiteId;
+            return $http.put(url, website);
         }
 
         function deleteWebsite(websiteId) {
-            for(var i in websites) {
-                if(websites[i]._id == websiteId) {
-                    websites.splice(i,1);
-                }
-            }
-        }
-
-        function websiteWithName(userId, name) {
-            for(var i in websites) {
-                if(websites[i].developerId == userId && websites[i].name == name) {
-                    return true;
-                }
-            }
-            return false;
+            var url = "/api/website/" + websiteId;
+            return $http.delete(url);
         }
 
         return {
@@ -78,8 +30,7 @@
             findWebsiteById: findWebsiteById,
             findWebsitesByUser: findWebsitesByUser,
             updateWebsite: updateWebsite,
-            deleteWebsite: deleteWebsite,
-            websiteWithName: websiteWithName
+            deleteWebsite: deleteWebsite
         }
     }
 
