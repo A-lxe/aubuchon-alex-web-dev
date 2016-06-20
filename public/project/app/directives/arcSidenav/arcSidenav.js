b1722b380a6b62ff1f8f67b7593e69af369b8f41
A-lxe <alex@alxe.co>
2016-06-20 01:52:49 -0400

    Project menu and sidenave coming along

--- public/project/app/directives/arcSidenav/arcSidenav.js
+++ public/project/app/directives/arcSidenav/arcSidenav.js
@@ -2,6 +2,13 @@ angular.module('Arcus')
     .directive('arcSidenav', function() {
         return {
             restrict: 'E',
-            templateUrl: 'app/directives/arcSidenav/sidenav.html'
+            templateUrl: 'app/directives/arcSidenav/sidenav.html',
+            controller: ['$scope', '$mdSidenav', ctrl]
         };
+
+        function ctrl($scope) {
+            $scope.entries = [
+                {name: 'Home', url: '#/', icon: 'home'}
+            ];
+        }
     });
